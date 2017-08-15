9fde5ba2e6427791299463b4483470f4fba5b510
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2017-08-15 16:02:32 +0200

    [Player] Add the player skeleton

---
+++ fr/tvbarthel/musictower/ai/Player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self):
+        self.dna = []
+        self.score = 0
+
+    def get_score(self):
+        return self.score
+
+    def get_dna(self):
+        return self.dna
