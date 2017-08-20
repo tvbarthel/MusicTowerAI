9ffd1059b151e38bac3c670068beba70f0b05309
tbarthel-fr <thomas.barthelemy.utc@gmail.com>
2017-08-20 10:05:33 +0200

    [Local] add possibility to run X times a local generation

--- fr/tvbarthel/musictower/ai/Runner.py
+++ fr/tvbarthel/musictower/ai/Runner.py
@@ -1,4 +1,4 @@
-from DeviceBot import DeviceBot
+from LocalBot import LocalBot
 from PopulationPersistor import PopulationPersistor
 
 
@@ -8,7 +8,7 @@ class Runner:
     """
 
     def __init__(self):
-        self.bot = DeviceBot()
+        self.bot = LocalBot()
         self.persistor = PopulationPersistor()
         pass
 
@@ -23,18 +23,47 @@ class Runner:
         population = self.persistor.load_population(input)
 
         while True:
-            print "new generation =============>"
-            # play every players
-            players = population.get_players()
-            for player in players:
-                player.score = self.bot.play(player.dna)
-                print str(player)
+            population = self.proceed_to_next_generation(output, population)
 
-            # store current population
-            print "Generation " + str(population)
-            self.persistor.save_population(population, output)
+    def run_simulation(self, input, output, count, times):
+        """
+               Run a complete simulation
+               :param input: path of the file for the first generation
+               :param output: path for storing each generation
+               :param count: number max of generation
+               :param times: number of times the simulation must be launched
+               :return: nothing.
+               """
+        average_generation = 0
+        for j in range(times):
+            population = self.persistor.load_population(input)
+            successful_generation = count
+            for i in range(count):
+                try:
+                    population = self.proceed_to_next_generation(output, population)
+                except ValueError:
+                    successful_generation = i
+                    break
+            average_generation += successful_generation
+        print "Average: " + str(average_generation / times)
 
-            # move on to the next generation
-            population = population.next_generation()
+    def proceed_to_next_generation(self, output, population):
+        """
+        Make the current generation live and reproduce to give birth to the next generation.
+        :param output: output where to store given generation once lived.
+        :param population: current generation.
+        :return: new generation born from the current one.
+        """
+        print "new generation =============>"
+        # play every players
+        players = population.get_players()
+        for player in players:
+            player.score = self.bot.play(player.dna)
+            print str(player)
 
-            print "new generation <=============="
+        # store current population
+        print "Generation " + str(population)
+        self.persistor.save_population(population, output)
+        # move on to the next generation
+        print "new generation <=============="
+        return population.next_generation()
